b96b4060a0b55da9c5f93300fcb610b575bc670b
CrackedPotato <arnavdrsk7633@gmail.com>
2025-04-10 19:01:26 +0000

    PROPS: Add missing lhdc props

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -263,5 +263,6 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/sm7550-common/BoardConfigVendor.mk
