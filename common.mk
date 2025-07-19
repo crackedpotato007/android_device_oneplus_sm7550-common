2109088b1db272947416fee5fb3f1f360e880210
Michael Bestas <mkbestas@lineageos.org>
2025-07-19 10:55:59 +0300

    sm7550-common: Migrate QTI power HAL to soong_config_set
    Change-Id: Ib80c15ee523130011147d378f665592dae501ec2

--- common.mk
+++ common.mk
@@ -263,6 +263,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/crow/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+$(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
+
 # QSPA
 PRODUCT_PACKAGES += \
     vendor.qti.qspa-service
